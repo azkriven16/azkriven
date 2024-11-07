675ee3d035e9b4777910925d2d1eace85037908e
euger <azkriven16@gmail.com>
2024-11-07 08:56:13 +0800

    feat: updated og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,14 +27,7 @@ export const metadata: Metadata = {
         siteName: `${SITE_CONFIG.firstName}'s Portfolio`,
         locale: "en_US",
         type: "website",
-        images: [
-            {
-                url: "/screenshot-desktop.png",
-                width: 1200,
-                height: 630,
-                alt: `${SITE_CONFIG.firstName}'s Portfolio Preview`,
-            },
-        ],
+        images: `${baseURL}api/og`,
     },
     twitter: {
         card: "summary_large_image",
