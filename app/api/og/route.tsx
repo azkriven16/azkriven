b3b7e51eb77d2709d9d314d09432726a8457b0ae
euger <azkriven16@gmail.com>
2024-11-07 09:12:06 +0800

    feat: added subheadings to og image

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -8,31 +8,47 @@ export async function GET() {
                     height: "100%",
                     width: "100%",
                     display: "flex",
-                    flexDirection: "row",
+                    flexDirection: "column",
                     alignItems: "center",
                     justifyContent: "center",
                     backgroundColor: "#000",
                     color: "#fff",
-                    fontSize: 100,
-                    fontWeight: 600,
                 }}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="200"
-                    height="200"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="icon icon-tabler icons-tabler-outline icon-tabler-picnic-table"
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        fontSize: 100,
+                        fontWeight: 600,
+                    }}
                 >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M16 7l2 9m-10 -9l-2 9m-1 -9h14m2 5h-18" />
-                </svg>
-                zkriven
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="200"
+                        height="200"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-picnic-table"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16 7l2 9m-10 -9l-2 9m-1 -9h14m2 5h-18" />
+                    </svg>
+                    zkriven
+                </div>
+                <div
+                    style={{
+                        fontSize: 25,
+                    }}
+                >
+                    web developer / software engineer
+                </div>
             </div>
         ),
         {
