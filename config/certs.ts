915abf6f2d20677a2f8c6564e7901496ecb14dcb
euger <azkriven16@gmail.com>
2024-11-12 20:29:53 +0800

    chore: updated certs description

--- config/certs.ts
+++ config/certs.ts
@@ -4,7 +4,7 @@ export const CERTS = [
         href: "https://www.testdome.com/certificates/530156ebb39f43c5a407bb157b414413",
         date: "Aug 1, 2022",
         description:
-            "Achieved a Javascript certification showcasing advanced skills in scripting and web interactivity from",
+            "Achieved a Javascript certification showcasing advanced skills in scripting and web interactivity from TestDome",
         certSource: "https://www.testdome.com/",
         certOrg: "TestDome",
     },
@@ -13,7 +13,7 @@ export const CERTS = [
         href: "https://www.testdome.com/certificates/b42958b8c828498782f72c304e0f7d24",
         date: "Oct 8, 2023",
         description:
-            "Attained a Typescript certification validating expertise in statically-typed JavaScript from",
+            "Attained a Typescript certification validating expertise in statically-typed JavaScript from TestDome",
         certSource: "https://www.testdome.com/",
         certOrg: "TestDome",
     },
@@ -22,7 +22,7 @@ export const CERTS = [
         href: "https://www.testdome.com/certificates/da2507e9432941068913931a9ce1dc47",
         date: "Oct 8, 2023",
         description:
-            "Secured a React certification indicating proficiency in building interactive user interfaces from",
+            "Secured a React certification indicating proficiency in building interactive user interfaces from TestDome",
         certSource: "https://www.testdome.com/",
         certOrg: "TestDome",
     },
@@ -31,8 +31,8 @@ export const CERTS = [
         href: "https://azkriven.vercel.app/cert.png",
         date: "Sep 8, 2022",
         description:
-            "Completed an Internship certification, showcasing practical experience and skills gained in a professional work environment from",
+            "Completed an Internship certification, showcasing practical experience and skills gained in a professional work environment from Digipay",
         certSource: "https://www.testdome.com/",
-        certOrg: "TestDome",
+        certOrg: "Digipay",
     },
 ];
