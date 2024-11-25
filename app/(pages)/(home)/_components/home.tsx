20e4ec192cd829c7dc1be75b1e9a21cad933f32c
euger <azkriven16@gmail.com>
2024-11-25 11:30:00 +0800

    feat: added contact page

--- app/(pages)/(home)/_components/home.tsx
+++ app/(pages)/(home)/_components/home.tsx
@@ -5,6 +5,7 @@ import { Hero } from "@/components/custom/hero";
 import { Projects } from "@/components/custom/projects";
 import { Section } from "@/components/custom/section";
 import { TechStack } from "@/components/custom/techstack";
+import { FadeText } from "@/components/ui/fade-text";
 import useScrollToTop from "@/hooks/scroll-to-top";
 
 export const HomeComponent = () => {
@@ -16,14 +17,15 @@ export const HomeComponent = () => {
             <Projects />
             <Section>
                 <div className="space-y-20 md:space-y-40">
-                    <p className="text-2xl md:text-4xl">
-                        I strive to bring innovation and creativity to every
+                    <FadeText
+                        className="text-2xl md:text-4xl"
+                        text=" I strive to bring innovation and creativity to every
                         project I undertake. Our team of experts works closely
                         with our clients to understand their needs and deliver
                         outstanding solutions. I are dedicated to creating
-                        products that exceed our clients' expectations. HTML5
-                        CSS3
-                    </p>
+                        products that exceed our clients' expectations."
+                    />
+                    <p></p>
                     <TechStack />
                 </div>
             </Section>
