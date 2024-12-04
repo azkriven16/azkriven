cefbe7d35730e1bbb0291a52cfb587bb4e7de0ca
euger <azkriven16@gmail.com>
2024-12-04 08:15:46 +0800

    updated loading

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -1,7 +1,5 @@
+import Loader from "@/components/loader";
+
 export default function Loading() {
-    return (
-        <div className="p-5 flex items-center justify-center min-h-screen text-center">
-            Loading...
-        </div>
-    );
+    return <Loader />;
 }
